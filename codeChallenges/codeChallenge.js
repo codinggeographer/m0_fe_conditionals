2692134020fab266e2c10f2f6a5ab809930b1890
Natalie Rodriguez <codinggeographer@gmail.com>
2023-04-19 20:20:27 -0600

    complete mild challenge by adding if / else statement to check remainder after dividing number by 2;

--- codeChallenges/codeChallenge.js
+++ codeChallenges/codeChallenge.js
@@ -1,8 +1,12 @@
 // mild challenge
-var number = 2
-
+// var number = 2
+// var number = 5
+// the if statement is taking the assigned number, dividing it by 2
+// and checks if the remainder of the division is equal to 0. This would
+// mean that it is an even number. The else prints odd because
+// the remaining amount of number / 2 != 0
 if (number % 2==0) {
-    console.log("The number is even")
-} else if (number % 2!=0) {
-    console.log("The number is odd")
+    console.log("even");
+} else {
+    console.log("odd");
 }
